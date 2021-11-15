7660f1ce56a8ab41aa92752f43281dc415127db0
seesea <seesea@seeseas-iMac.local>
2021-11-15 14:09:25 +0800

    update token swap program id to PSwapkDTLvV5R8BHS31nypM48TsDV5qN9AAQzpqBdYX

--- token-swap/program/src/lib.rs
+++ token-swap/program/src/lib.rs
@@ -16,4 +16,4 @@ mod entrypoint;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("SwapsVeCiPHMUAtzQWZw7RjsKjgCjhwU55QGu4U1Szw");
+solana_program::declare_id!("PSwapkDTLvV5R8BHS31nypM48TsDV5qN9AAQzpqBdYX");
